40b33136522b5449ffa0c1516ab00144e9ef05a2
Luis Alejandro Vargas Ramos <lavargasra@unal.edu.co>
2023-09-09 09:07:46 -0500

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -4,72 +4,40 @@ import seaborn as sn
 import pandas as pd
 import re
 
-
-def gettimeanddate(string):
-    string = string.split(',')
-    date, time = string[0], string[1]
-    time = time.split('-')
-    time = time[0].strip()
-
-    return date+" "+time
-
-
-def getstring(text):
-    return text.split('\n')[0]
-
-
-def preprocess(data):
-
-    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
-    messages = re.split(pattern, data)[1:]
-    dates = re.findall(pattern, data)
-
-    df = pd.DataFrame({'user_messages': messages,
-                       'message_date': dates})
-
-    df['message_date'] = df['message_date'].apply(
-        lambda text: gettimeanddate(text))
-    df.rename(columns={'message_date': 'date'}, inplace=True)
-
+def preprocess(text):
+    # Initialize empty lists to store extracted data
+    dates = []
+    times = []
     users = []
     messages = []
 
-    for message in df['user_messages']:
+    for string in text:
+        # Split the string by the first '-' to separate date and message
+        parts = string.split('-', 1)
+        if len(parts) == 2:
+            date, message = parts
+            # Split the date by ',' to separate date and time
+            date_parts = date.split(',')
+            if len(date_parts) == 2:
+                date = date_parts[0].strip()
+                time = date_parts[1].strip()
+                # Extract user from the message
+                user_message_parts = message.split(':', 1)
+                if len(user_message_parts) == 2:
+                    user = user_message_parts[0].strip()
+                    message = user_message_parts[1].strip()
+                    dates.append(date)
+                    times.append(time)
+                    users.append(user)
+                    messages.append(message)
 
-        entry = re.split('([\w\W]+?):\s', message)
-        if entry[1:]:
-            users.append(entry[1])
-            messages.append(entry[2])
+    # Create a DataFrame
+    data = {
+        'Date': dates,
+        'Time': times,
+        'User': users,
+        'Message': messages
+    }
 
-        else:
-            users.append('Group Notification')
-            messages.append(entry[0])
-
-    df['User'] = users
-    df['message'] = messages
-
-    df['message'] = df['message'].apply(lambda text: getstring(text))
-
-    df = df.drop(['user_messages'], axis=1)
-    df = df[['message', 'date', 'User']]
-
-    df = df.rename(columns={'message': 'Message',
-                            'date': 'Date'})
-
-    df['Only date'] = pd.to_datetime(df['Date']).dt.date
-
-    df['Year'] = pd.to_datetime(df['Date']).dt.year
-
-    df['Month_num'] = pd.to_datetime(df['Date']).dt.month
-
-    df['Month'] = pd.to_datetime(df['Date']).dt.month_name()
-
-    df['Day'] = pd.to_datetime(df['Date']).dt.day
-
-    df['Day_name'] = pd.to_datetime(df['Date']).dt.day_name()
-
-    df['Hour'] = pd.to_datetime(df['Date']).dt.hour
-
-    df['Minute'] = pd.to_datetime(df['Date']).dt.minute
-
+    df = pd.DataFrame(data)
     return df
